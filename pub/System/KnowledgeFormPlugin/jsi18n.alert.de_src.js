53b82425fe02cfb5d42e2734504da02579ba44e2
Daniel Supplieth <supplieth@modell-aachen.de>
2016-08-18 11:41:02 +0200

    Check mandatory group handler should be called from modac skin.
    Ticket: #14065
    Releas-Note: none

--- pub/System/KnowledgeFormPlugin/jsi18n.alert.de_src.js
+++ pub/System/KnowledgeFormPlugin/jsi18n.alert.de_src.js
@@ -1,3 +1,3 @@
 jsi18n.add('alert', {
-  "You have not filled out on of the mandatory fields: '[_1]'.": "Sie haben keins der Pflichtfelder '[_1]' ausgefüllt.",
+  "You have not filled out on of the mandatory fields: '[_1]'.": "Sie müssen mindestens ein Feld der Pflichtfelder '[_1]' ausfüllen.",
 });
